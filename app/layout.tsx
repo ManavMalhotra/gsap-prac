f8c8d2212465f5c2343de43636296dc493351726
Manav Malhotra <manavmalhotrafrnd4u@gmail.com>
2024-09-02 16:44:19 +0530

    Update npm dependencies and fonts
    This commit updates the npm dependencies in the `package.json` file, including adding the `@gsap/react` and `gsap` packages. It also updates the fonts used in the `app/layout.tsx` file, replacing the `Inter` font with `Roboto` and `Playfair_Display`.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,20 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { Roboto } from "next/font/google";
+import { Playfair_Display } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+// const inter = Inter({ subsets: ["latin"] });
+const roboto = Roboto({
+  weight: '400',
+  subsets: ['latin'],
+})
+
+const dmSerifText = Playfair_Display({
+  subsets: ['latin'],
+  weight: '400',
+
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +28,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={dmSerifText.className}>{children}</body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,143 @@
+"use client"
+import { useEffect, useRef } from "react";
+import { gsap } from "gsap";
+import ScrollTrigger from "gsap/ScrollTrigger";
+import { useGSAP } from "@gsap/react";
+import Link from "next/link";
 import Image from "next/image";
+import img1 from "@/public/img1.jpeg"
+import img2 from "@/public/img2.jpeg"
+import img3 from "@/public/img3.webp"
+import img4 from "@/public/img4.webp"
+import male from "@/public/male.jpeg"
+import female from "@/public/female.jpeg"
+import logo from "@/public/logo.png"
+
+gsap.registerPlugin(ScrollTrigger);
 
 export default function Home() {
+  const b1 = useRef(null);
+  const b2 = useRef(null);
+
+  useGSAP(() => {
+    const t1_img = gsap.timeline({ ease: "Power1.easeOut" })
+
+    t1_img.to(".men .image-overlay", {
+      duration: 1,
+      y: "100%",
+    })
+
+    t1_img.to(".women .image-overlay", {
+      duration: 1,
+      y: "100%",
+    }, "-=.7")
+
+    t1_img.from(".img", {
+      opacity: 0,
+      duration: 0.4,
+      scale: 1.2,
+      stagger: .2
+    })
+
+    t1_img.from(".text", {
+      opacity: 0,
+      duration: 0.4,
+      y: 20,
+    })
+
+    t1_img.to(".content", {
+      // y: "80px",
+      duration: 1,
+      marginTop: "0"
+    })
+
+    const t2_header = gsap.timeline({ ease: "Power1.easeInOut" })
+
+    t2_header.from("nav", {
+      delay: 3.5,
+      duration: 0.5,
+      y: "-80",
+    })
+
+    t2_header.from(".logo", {
+      opacity: 0,
+      y: -20,
+      duration: 0.5,
+    })
+
+    t2_header.from("nav li", {
+      opacity: 0,
+      y: -20,
+      duration: 0.5,
+      stagger: 0.2, // Stagger effect
+    }, "-=.5")
+
+
+  });
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="">
+      <nav className="flex justify-between px-16 py-4 text-white bg-black content mt-[-80px]">
+        {/* <div className="w-8 h-8 bg-gray-500 logo"></div> */}
+
+        <Image src={logo} alt="logo" className="w-auto h-8 logo" ></Image>
+
+        <ul className="flex gap-12">
+          <li className="cursor-pointer" >Home</li>
+          <li className="cursor-pointer">About Us</li>
+          <li className="cursor-pointer">Our Services</li>
+          <li className="cursor-pointer">Contact Us</li>
+        </ul>
+      </nav>
+
+      <section className="flex">
+        <div className="men w-[70%] h-[92vh]  relative overflow-hidden">
+          <div className="absolute top-0 left-0 w-full h-full bg-black image-overlay"></div>
+          <Image src={male} alt="wolf" objectFit="contain" className="img"
+            style={{
+              filter: "brightness(0.5) grayscale(1) contrast(0.9) ",
+              width: "100%",
+            }}
+          ></Image>
+          <div className="absolute w-full text-center text-white text bottom-28">
+            <h1 className="text-6xl uppercase">Men</h1>
+            <h1>Fashion</h1>
+          </div>
+
+          <div className=" shop-now absolute transition-all px-2 py-2 top-[50%] left-[50%] border border-white border-solid translate-x-[-80px] opacity-0 hover:opacity-100 ">
+            <Link href="/" className="text-base text-white uppercase ">
+              Shop Now
+            </Link>
+          </div>
+
         </div>
-      </div>
 
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+        <div className="women w-[70%] h-[92vh] relative overflow-hidden">
+          <Image src={img3} objectFit="contain" alt="women" className="img"
+            style={{
+              filter: "brightness(0.5) grayscale(1) contrast(0.9) ",
+              width: "100%",
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
+            }}
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
+          ></Image>
+          <div className="absolute top-0 left-0 w-full h-full bg-black image-overlay"></div>
+          <div className="absolute w-full text-center text-white bottom-28 text">
+            <h1 className="text-6xl uppercase ">Women</h1>
+            <h1>Fashion</h1>
+          </div>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
+          <div className=" shop-now absolute transition-all px-2 py-2 top-[50%] left-[50%] border border-white border-solid translate-x-[-80px] opacity-0 hover:opacity-100 ">
+            <Link href="/" className="text-base text-white uppercase ">
+              Shop Now
+            </Link>
+          </div>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
+
+        </div>
+
+
+      </section>
     </main>
   );
 }
